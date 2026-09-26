c372d0f4748afd4a89cff78a28dc43eb19746675
ahad <abdulahad95460@gmail.com>
2026-09-26 17:29:53 +0530

    define API route prefix in Express server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const primeRoutes = require("./routes/primeRoutes");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api", primeRoutes);
+
 
 app.get("/", (req, res) => {
   res.json({
